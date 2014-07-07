b6b9d4b3535648821bc3b05c82cb131ab2e1dcde
cuongwazza <cuongwazza@gmail.com>
2014-07-07 10:12:07 +0700

    Up
    Up laij

---
+++ app/database/migrations/2014_07_07_030950_create_users.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsers extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function($table)
+		{
+			$table->increments('id');
+			$table->string('firstname');
+			$table->string('lastname');
+			$table->string('email', 255)->unique();
+			$table->string('password');
+			$table->date('weddingdate');
+			$table->string('role');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}

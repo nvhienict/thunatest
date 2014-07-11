bfe2a66c6a1f3885c47f3d5c1c517c765ee80c81
phuocbinh171 <mtpbinh@gmail.com>
2014-07-11 13:38:30 +0700

    write a review interface
    write a review interface

--- app/routes.php
+++ app/routes.php
@@ -23,12 +23,16 @@ Route::resource('users', 'UserController');
 Route::get('/header-home',function(){
 	return View::make('template.header-home');
 });
+Route::get('/header-home1',function(){
+	return View::make('template.header-home1');
+});
 Route::get('/menu',function(){
 	return View::make('template.menu');
 });
 Route::get('/footer',function(){
 	return View::make('template.footer');
 });
+
 Route::get('/detail-vendor', function(){
 	return View::make('template.detail');
 });
@@ -37,4 +41,7 @@ Route::get('/khung', function(){
 });
 Route::get('/hellooo',function(){
 	return View::make('hellooo');
+});
+Route::get('/review',function(){
+	return View::make('template.reviews');
 });
